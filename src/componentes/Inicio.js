4323d28237f66d6b78792167b709dfe752e38a5c
JuanFelipeGir <juan.giraldo11@correo.tdea.edu.co>
2023-05-16 21:02:46 -0500

    Se cambio algo de barra, se organizo el carrousel

--- src/componentes/Inicio.js
+++ src/componentes/Inicio.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "../styles/inicio.css";
 import BarraBusqueda from './BarraBusqueda';
+import Carrouseluno from '../icons/carrouseluno.jpg';
+import Carrouseldos from '../icons/carrouseldos.jpg';
+import Carrouseltres from '../icons/carrouseltres.jpg';
 
 
 function Inicio() {
@@ -9,13 +12,13 @@ function Inicio() {
 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="..."></img>
+      <img src={Carrouseluno} class="d-block w-100" alt="..."></img>
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..."></img>
+      <img src={Carrouseldos} class="d-block w-100" alt="..."></img>
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..."></img>
+      <img src={Carrouseltres} class="d-block w-100" alt="..."></img>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
